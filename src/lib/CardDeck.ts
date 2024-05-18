6c93051b6fb5ffb791b9797f7bf82e400f0bb118
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-19 01:39:09 +0600

    level 2

---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,46 @@
+import playingCard from './playingCard.ts';
+
+class CardDeck{
+  private cards: playingCard[];
+
+  constructor() {
+    this.cards = this.createDeck();
+  }
+  private createDeck(): playingCard[] {
+    const suits = ['diams', 'hearts', 'clubs', 'spades'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const deck: playingCard[] = [];
+
+    for (const suit of suits) {
+      for (const rank of ranks) {
+        deck.push(new playingCard(rank, suit));
+      }
+    }
+    return deck;
+  }
+  public getCard(): playingCard {
+    if (this.cards.length === 0) {
+       alert('Карт не осталось');
+    }
+    const randomIndex = Math.floor(Math.random() * this.cards.length);
+    return this.cards.splice(randomIndex, 1)[0]; // Удаляем и возвращаем карту
+  }
+
+  public getCards (howMany: number): playingCard[] {
+    const cards: playingCard[] = [];
+    for(let i = 0; i < howMany; i++) {
+      const card = this.getCard();
+      if (card){
+        cards.push(card);
+      }else {
+        break;
+      }
+    }
+    return cards;
+  }
+
+}
+
+
+
+export default CardDeck;
---
+++ src/lib/playingCard.ts
@@ -0,0 +1,12 @@
+class playingCard {
+  public rank: string;
+  public suit: string;
+
+    constructor(rank: string, suit: string) {
+  this.rank = rank;
+  this.suit = suit;
+}
+}
+
+
+export default  playingCard;
